e9daa3f3b5cb329e7e1b4ba6dcdb7de6df38d397
José Pach <Paching12@icloud.com>
2025-11-11 22:13:49 -0600

    add new button type for "use current position"

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,6 +1,7 @@
 export const BUTTON_TYPES = {
   PRIMARY: "primary",
   BACK: "back",
+  POSITION: "position",
 } as const;
 
 export type ButtonTypeValues = (typeof BUTTON_TYPES)[keyof typeof BUTTON_TYPES];
