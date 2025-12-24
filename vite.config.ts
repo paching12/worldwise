f6ace1b36b213405ddde6f74f06d755ca351aa2a
José Pach <Paching12@icloud.com>
2025-12-24 12:14:25 -0600

    Add alias for the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@reducers": path.resolve(__dirname, "./src/reducers"),
+      "@actions": path.resolve(__dirname, "./src/actions"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
+});
