0387971bad4b060b6356b821599385110da9ae2e
José Pach <Paching12@icloud.com>
2026-02-16 18:51:36 -0600

    Updates

---
+++ assets/Logo-Be9D_My2.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-CgA2dcLt.js";const t="_logo_d1uor_1",r={logo:t},g=()=>o.jsx(s,{to:"/",children:o.jsx("img",{src:"/logo.png",alt:"Wordwide",className:r.logo})});export{g as L};
---
+++ assets/PageNotFound-BEFf8oct.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CgA2dcLt.js";const e=()=>o.jsx("div",{children:"Page Not Found"});export{e as default};
